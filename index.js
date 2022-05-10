d4ba506bb69828d6c6dbe630f8b237b3b76fc5a1
honeykhra <honeykhra@gmail.com>
2022-05-10 20:51:59 +0530

    some

--- index.js
+++ index.js
@@ -43,20 +43,20 @@ app.get("/", (req, res) => {
   res.json("Welcome to  my cluimate Change api news");
 });
 
-// app.get("/news", (req, res) => {
-//   axios
-//     .get("https://www.theguardian.com/environment/climate-crisis")
-//     .then((response) => {
-//       const html = response.data;
-//       const $ = cheerio.load(html);
-//       $('a:contains("climate")', html).each(function () {
-//         const title = $(this).text();
-//         const url = $(this).attr("href");
-//         articles.push({ title, url });
-//       });
-//       res.json(articles);
-//     })
-//     .catch((err) => err);
-// });
+app.get("/news", (req, res) => {
+  //   axios
+  //     .get("https://www.theguardian.com/environment/climate-crisis")
+  //     .then((response) => {
+  //       const html = response.data;
+  //       const $ = cheerio.load(html);
+  //       $('a:contains("climate")', html).each(function () {
+  //         const title = $(this).text();
+  //         const url = $(this).attr("href");
+  //         articles.push({ title, url });
+  //       });
+         res.json(articles);
+  //     })
+  //     .catch((err) => err);
+});
 
 app.listen(PORT, () => console.log("server running on port " + PORT));
